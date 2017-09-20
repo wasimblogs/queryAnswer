fd92afb7786cb4c1e150b0d5481f9bd40861b808
Wasim Akram Khan <wasimblogs4@hotmail.com>
2017-09-20 10:30:35 +0545

    Add files via upload

--- bagofWords.py
+++ bagofWords.py
@@ -6,6 +6,8 @@ from nltk.corpus import stopwords
 import numpy as np
 import math
 from nltk.util import ngrams
+import time  # used to time profile methods
+from nltk.corpus import wordnet
 
 """
 Things noticed and things to treat with caution
@@ -27,6 +29,29 @@ stop.append(".")
 stop.append(",")
 
 
+def timeit(method):
+    """
+    Used for time profiling functions and methods
+    :param method:
+    :return:
+    """
+
+    def timed(*args, **kwargs):
+        timeStart = time.time()
+        result = method(*args, **kwargs)
+        timeEnd = time.time()
+
+        if 'log_time' in kwargs:
+            name = kwargs.get('log_name', method.__name__.upper())
+            kwargs['log_time'][name] = int((timeEnd - timeStart) * 1000)
+        else:
+            print('%r %2.2f ms' % (method.__name__, (timeEnd - timeStart) * 1000))
+        return result
+
+    return timed
+
+
+@timeit
 def vocabularyFromCorpus(inputString, showDebugInfo=False, computeBigram=False):
     """
     Builds a vocabulary from coprus
@@ -97,6 +122,7 @@ def vocabularyFromCorpus(inputString, showDebugInfo=False, computeBigram=False):
     return sentTokenizedFiltered, vocab
 
 
+@timeit
 def vectorize(sentTokens, vocab, showDebugInfo=True):
     """
     # To compute the frequency occurence of words
@@ -123,6 +149,23 @@ def vectorize(sentTokens, vocab, showDebugInfo=True):
     return count, presence
 
 
+@timeit
+def normalizeVector(vector):
+    """
+    Returns normalized vector
+    """
+    # Checking for divide by zero error
+    sum = (vector.sum(axis=0))
+    if sum == 0:
+        return vector
+
+    else:
+        rootOfSquareSum = np.sqrt((vector * vector).sum(axis=0))
+        vector = vector / rootOfSquareSum
+        return vector
+
+
+@timeit
 def tfidf(count, presence, vocab, sentTokens):
     """
     # Compute Term Frequency Inverse Document Frequency for given corpus
@@ -164,21 +207,23 @@ def tfidf(count, presence, vocab, sentTokens):
     return tfidfVec
 
 
-def normalizeVector(vector):
+def calcSynonyms(word):
     """
-    Returns normalized vector
+    :param word: word for which synonym is to be calculated
+    :return: list of synonym for the input word
     """
-    # Checking for divide by zero error
-    sum = (vector.sum(axis=0))
-    if sum == 0:
-        return vector
 
-    else:
-        rootOfSquareSum = np.sqrt((vector * vector).sum(axis=0))
-        vector = vector / rootOfSquareSum
-        return vector
+    synonyms = []
+
+    for syn in wordnet.synsets(word):
+        for l in syn.lemmas():
+            word = l.name()
+            synonyms.append(word)
+
+    return synonyms
 
 
+@timeit
 def analyze(tfidfVec, sentTokens, vocab, nTopWords=3, showDebugInfo=True):
     """Finds the most important words in a sentence"""
 
@@ -203,7 +248,8 @@ def analyze(tfidfVec, sentTokens, vocab, nTopWords=3, showDebugInfo=True):
             print("\n\n", result, "----", sentence)
 
 
-def preProcessSent(sentence, computeBigram=False, showDebugInfo=False):
+@timeit
+def preProcessSent(sentence, computeBigram=False, showDebugInfo=False, insertSynonym=True):
     """
     To lower. Stop word removal. Lemmatize. Return lemmatized input
     :param sentence: input which needs to be preprocessed
@@ -219,6 +265,7 @@ def preProcessSent(sentence, computeBigram=False, showDebugInfo=False):
 
     wordTokens = wordTokenizer.tokenize(sentence)
     wordTokens = [lemmatizer.lemmatize(word) for word in wordTokens if word not in stop]
+    listOfWords = set(wordTokens)  # used to calculate synonyms
 
     if computeBigram:
         bigrams = list(ngrams(wordTokens, 2))
@@ -228,6 +275,23 @@ def preProcessSent(sentence, computeBigram=False, showDebugInfo=False):
 
             if showDebugInfo:
                 print("Bigram sentence: ", wordTokens)
+
+    # If insert synonyms of words present in the sentence
+    if insertSynonym:
+        synonyms = []
+        for word in listOfWords:
+            syns = calcSynonyms(word)
+            for syn in syns:
+                synonyms.append(syn)
+
+        # Do not repeat synonyms
+        synonyms = list(set(synonyms))
+        for syn in synonyms:
+            if syn in vocab:
+                wordTokens.append(syn)
+
+    if showDebugInfo:
+        print(wordTokens)
 
     return wordTokens
 
@@ -239,7 +303,14 @@ def userInput():
     pass
 
 
-def vectorizeSentence(wordTokens, topNMatch=10):
+@timeit
+def vectorizeUserQuery(wordTokens, topNMatch=10):
+    """
+    Vectorize user query and compute similarity with database questions
+    :param wordTokens: tokenized user query
+    :param topNMatch: how many best matches to show
+    :return: TF-IDF of user query
+    """
     # print("Length of vocabulary : ", len(vocab))
     # print(idfVec)
 
@@ -271,7 +342,7 @@ def vectorizeSentence(wordTokens, topNMatch=10):
         max = np.max(scores)
         index = scores.index(max)
         scores[index] = 0
-        print(i+1, "\tBest matches : ", max, inputSentence[index])
+        print(i + 1, "\tBest matches : ",index, max, inputSentence[index])
         # except:
         #     print("You broke the system. No matches found.")
 
@@ -279,7 +350,12 @@ def vectorizeSentence(wordTokens, topNMatch=10):
     # Reconstruct sentence to check if you counted right
 
 
+@timeit
 def readCorpus(filename):
+    """
+    :param filename: a question is written as a line in file. Reads that file
+    :return: returns list of questions
+    """
     corpus = []
     string = ""
     with open(filename, "r") as file:
@@ -287,12 +363,40 @@ def readCorpus(filename):
             # print("Line:",line)
             corpus.append(line)
             string += line
-        print(len(corpus))
+        print("No. of questions:", len(corpus))
 
     file.close()
     return string, corpus
 
 
+@timeit
+def readAnswers(filename, showDebugInfo=False):
+    """
+    :param filename: a question is written as a line in file. Reads that file
+    :return: returns list of questions
+    """
+    corpus = []
+    string = ""
+    with open(filename, "r") as file:
+        for line in file:
+            # print("Line:",line)
+            corpus.append(line)
+            string += line
+
+    file.close()
+
+    answers = string.split("###")
+
+    if showDebugInfo:
+        print("No. of answers", len(answers))
+        for answer in answers:
+            print(answers.index(answer), answer)
+            print("\n")
+
+    return answers
+
+
+@timeit
 def buildQAModel(corpus, showDebugInfo=False, computeBigram=False):
     """
     builds QA model from given corpus
@@ -300,20 +404,33 @@ def buildQAModel(corpus, showDebugInfo=False, computeBigram=False):
     :return:
     """
 
+    print("1/4 - Computing vocabulary from corpus")
     sentTokenizedFiltered, vocab = vocabularyFromCorpus(corpus, showDebugInfo=showDebugInfo,
                                                         computeBigram=computeBigram)
+
+    print("2/4 - Vectorizing queries")
     count, presence = vectorize(sentTokenizedFiltered, vocab)
+
+    print("3/4 - Bag of words with weight of words computed using TD-IDF")
     tfidfVec = tfidf(count, presence, vocab, sentTokenizedFiltered)
-    analyze(tfidfVec, sentTokenizedFiltered, vocab, 3, showDebugInfo=showDebugInfo)
+
+    print("4/4 - Finding most important words in queries")
+    analyze(tfidfVec, sentTokenizedFiltered, vocab, 3, showDebugInfo=True)
 
 
+@timeit
 def testAgainstQueries(computeBigram=False):
     from nltk.corpus import abc, genesis
     trainText = abc.raw("science.txt")
     trainText = genesis.raw("english-web.txt")
 
-    string, input = readCorpus("faq.txt")
+    string, input = readCorpus("ncellfaq.txt")
     buildQAModel(input, computeBigram=computeBigram)
+
+    string, input = readCorpus("ncellanswer.txt")
+    buildQAModel(input, computeBigram=computeBigram)
+
+    print(len(vocab))
 
     queries = ["How do I get ATM card?",
                "What is NRI ?",
@@ -336,13 +453,29 @@ def testAgainstQueries(computeBigram=False):
     #            "Where do I find salvation on this earth?",
     #            "What is God?"]
 
+    queries = [
+        "How to activate 4G?",
+        "What are cities with 4G network coverage?",
+        "Do you sell phones?",
+        "Where can I buy ringtone?",
+        "What happens if I lose my SIM card?",
+        "What is the difference between 4G and 3G sim card?",
+        "What is the difference between tariffs of 3G and 4G sim card?",
+        "I get busy all the time ringtone?"]
+
     for query in queries:
         processedQuery = preProcessSent(query, computeBigram=computeBigram)
         print("\n\n", "*" * 60, "\n\nQuestion : ", query, "\nMatches...")
-        vectorizeSentence(processedQuery, 3)
+        vectorizeUserQuery(processedQuery, 3)
 
 
 if __name__ == "__main__":
-    testAgainstQueries(computeBigram=False)
-    # string, input = readCorpus("faq.txt")
-    # _, vocab = vocabularyFromCorpus(string, computeBigram=False)
+    testAgainstQueries(computeBigram=True)
+    # string, input = readCorpus("ncellfaq.txt")
+    # vocabularyFromCorpus(string, computeBigram=False,showDebugInfo=True)
+
+    # readAnswers("ncellanswer.txt")
+    # vocabularyFromCorpus(string, computeBigram=False,showDebugInfo=True)
+
+    # print(len(vocab))
+    # preProcessSent("What happens if I forget password?", showDebugInfo=True, insertSynonym=True)
