206f3d13754911a73ae9613e426f9ae69c9e0135
Wasim Akram Khan <wasimblogs4@hotmail.com>
2017-09-22 11:21:55 +0545

    Added cross validation

--- textClassifier.py
+++ textClassifier.py
@@ -8,6 +8,8 @@ from bagofWords import timeit
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 from nltk import bigrams
+from nltk.tokenize import WordPunctTokenizer
+
 
 def normalizeVector(vector):
     """
@@ -90,37 +92,56 @@ def vectorize(sentTokens, vocab, showDebugInfo=True):
 
 
 @timeit
-def collectData(nTopReviews=10, computeBigram = False):
+def collectData(nTopReviews=10, computeBigram=False, lowFrequencyThresh=1):
     """
     Collect data from corpus. Form vocab. Stemming and stopword removal
     :return : listOfText, vocab
+    :param lowFrequencyThresh: remove words with frequency equal or below this value
     """
 
     # stopword collection
-    stopwordString = " - , . '  : of and a the to is s it that ( ) as film with for hi thi i he but be on movi are t by one an who habe you from at wa they ha her all charact there ? so out about up what"
+    stopwordString = "have films was ; -- movie or this in - , . '  : of and a the to is s it that ( ) as film with for hi thi i he but be on movi are t by one an who habe you from at wa they ha her all charact there ? so out about up what"
+    pronoun = "all another any anybody anyone anything both each either everybody everyone everything few he her hers herself him himself his I it its itself me my myself other our ours ourselves she somebody someone something that their theirs them themselves these they this those us we what whatever which whichever who whoever whom whomever whose you your yours yourself yourselves"
+
+    pronoun = pronoun.split()
     stopword = stopwordString.split(" ")
     stopword.append('"')
+    stopword = stopword + pronoun
 
     # Initialize stemmer and lemmatizer
     lemmatizer = WordNetLemmatizer()
     stemmer = PorterStemmer()
+    wordTokenizer = WordPunctTokenizer()
 
-    # Read all the movie review data from two folders pos and neg
-    category = (movie_reviews.categories())
-    global  documents
+    # # Read all the movie review data from two folders pos and neg
+    global documents
     documents = []
-    for category in movie_reviews.categories():
-        for file in movie_reviews.fileids(category):
-            review = movie_reviews.words(file)
-            reviewProcessed = [stemmer.stem(word) for word in review]
-            documents.append((reviewProcessed, category))
+    # category = (movie_reviews.categories())
+    # for category in movie_reviews.categories():
+    #     for file in movie_reviews.fileids(category):
+    #         review = movie_reviews.words(file)
+    #         reviewProcessed = [stemmer.stem(word) for word in review]
+    #         documents.append((reviewProcessed, category))
+
+    # Read all the reviews from positive.txt and negative.txt
+    short_pos = open("Dataset/positive.txt", "r").read()
+    short_neg = open("Dataset/negative.txt", "r").read()
+
+    for r in short_pos.split('\n'):
+        wordTokens = wordTokenizer.tokenize(r)
+        documents.append((wordTokens, "pos"))
+
+    for r in short_neg.split('\n'):
+        wordTokens = wordTokenizer.tokenize(r)
+        documents.append((wordTokens, "neg"))
 
     print("Read reviews from database")
+
     # Shuffle movie review order so that pos and neg reviews are interspersed
     random.shuffle(documents)
 
-    if nTopReviews > len(documents):
-        nTopReviews = len(documents) - 1
+    if nTopReviews > len(documents) or nTopReviews == 0:
+        nTopReviews = len(documents)
 
     listOfReviews = []
     vocab = []
@@ -141,7 +162,7 @@ def collectData(nTopReviews=10, computeBigram = False):
     print("Vocabulary size : ", len(vocab))
 
     # Add one time occuring word to stopword list
-    oneTimeWords = [key for key in counts if counts[key] == 1]
+    oneTimeWords = [key for key in counts if counts[key] <= lowFrequencyThresh]
     stopword = stopword + oneTimeWords
 
     # Filter stopwords and rare words from vocab
@@ -153,7 +174,7 @@ def collectData(nTopReviews=10, computeBigram = False):
     totalNoWords = 0
     vocabBigram = []
     for sent in listOfReviews:
-        sentStopwordFiltered = [ word for word in sent if word not in stopword]
+        sentStopwordFiltered = [word for word in sent if word not in stopword]
 
         if computeBigram:
             bigram = list(bigrams(sentStopwordFiltered))
@@ -172,41 +193,77 @@ def collectData(nTopReviews=10, computeBigram = False):
     return listOfReviews, vocab
 
 
-def trainAndTest(listOfReviews, vocab):
+def trainAndTest(listOfReviews, vocab, kFold=5):
     """
     :param listOfReviews: list of text / emails / sentence / moview reviews
     :param vocab: vocab of corpus
+    :param kFold: k for cross validation
     :return:
     """
 
     # Obtaining TF IDF of movie reviews
-    print("Now on to vectorizing")
-    # tfidfVec = vectorize(listOfReviews[:nTopReviews], vocab, showDebugInfo=True)
-    tfidfVec = vectorize(listOfReviews, vocab, showDebugInfo=True)
+    # print("Now on to vectorizing")
+    # tfidfVec = vectorize(listOfReviews, vocab, showDebugInfo=True)
 
     # Getting feature, labels to train and test
     featureSets = []
     noOfReviews = len(listOfReviews)
     for i in range(0, noOfReviews):
-        vec = list(tfidfVec[i])
+        # vec = list(tfidfVec[i])
 
         feature = {}
-        for word, score in zip(vocab, vec):
-            feature[word] = score
+        # for word, score in zip(vocab, vec):
+        #     # feature[word] = score
+        #     feature[word] = word in listOfReviews[i]
+
+        for word in vocab:
+            feature[word] = word in listOfReviews[i]
 
         rating = documents[i][1]
         featureSets.append((feature, rating))
 
-    # Dividing data into trainSet and testSet
-    trainingSet = featureSets[int(0.5 * noOfReviews):]
-    testSet = featureSets[:int(0.5 * noOfReviews)]
 
-    # Traingin classifer and testing accuracy
-    classifier = nltk.NaiveBayesClassifier.train(trainingSet)
-    accuracy = nltk.classify.accuracy(classifier, testSet) * 100
-    print("Accuracy : {} %".format(accuracy))
+    # k fold cross validation
+    accuracySum = 0
+    for i in range(0, kFold):
+        random.shuffle(featureSets)
+        # Dividing data into trainSet and testSet
+        trainingSet = featureSets[int(0.5 * noOfReviews):]
+        testSet = featureSets[:int(0.5 * noOfReviews)]
+
+        # Traingin classifer and testing accuracy
+        classifier = nltk.NaiveBayesClassifier.train(trainingSet)
+
+        # To test if desired keywords caused the result or just random guesses
+        # (classifier.show_most_informative_features(100))
+        accuracy = nltk.classify.accuracy(classifier, testSet) * 100
+        print("Accuracy : {} %".format(accuracy))
+
+        accuracySum = accuracySum + accuracy
+
+    print("Average Accuracy : ", (accuracySum)/kFold)
+    return classifier
+
+
+def preprocessUserInput(inputString):
+    """
+    Test movie reviews from live users in production phase
+    :param inputString: text / movie review / email
+    :return: input ready for classifer
+    """
+    pass
+
+
+def testClassifer(inputQuery):
+    """
+    Testing classifier
+    :param inputQuery:
+    :return:
+    """
+    global classifier
+    pass
 
 
 if __name__ == '__main__':
-    listOfReviews, vocab = collectData(100, computeBigram=False)
-    trainAndTest(listOfReviews, vocab)
+    listOfReviews, vocab = collectData(1000, computeBigram=True, lowFrequencyThresh=1)
+    classifier = trainAndTest(listOfReviews, vocab,kFold=10)
